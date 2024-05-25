0b158dfcd1fcc363ef7ca3116f1d32b074f71792
githubscientist <sathish.datascientist@gmail.com>
2024-05-25 18:37:02 +0530

    settimeout

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-// console.log('hello world!');
+// create a small notification in the top of the screen.
+const notification = document.getElementById('notification');
 
-// console.log(window.document.querySelector('h1'));
+notification.textContent = 'This is a notification!';
 
-// console.log(window.location); // location object
-// console.log(window.location.host); // host property
-// console.log(window.location.href); // href property
-
-// window.location.href = 'https://www.google.com'; // redirect to google.com
-
-// console.log(window.innerHeight);
-
-// alert('hello world!');
-
-// const email = prompt('enter your email to subscribe to our newsletter');
-
-// console.log(email);
-
-// const isConfirm = confirm('are you sure you want to delete this item?');
-
-// if (isConfirm) {
-//     // delete item
-//     console.log('item deleted');
-// } else {
-//     console.log('item not deleted');
-// }
-
-// function sayHello() {
-//     prompt('enter your email to subscribe to our newsletter');
-// }
-
-// setTimeout(sayHello, 3000);
-// setInterval(sayHello, 3000);
+setTimeout(() => {
+    notification.classList.add('hide');
+}, 1000);
